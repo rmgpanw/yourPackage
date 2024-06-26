432da64ac2f8093fdc17bdc2dfd253e78ca4f378
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-26 13:37:48 +0100

    export function

--- R/get_tissues_for_gene.R
+++ R/get_tissues_for_gene.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' Get tissues where a gene has eQTLs.
+#'
+#' @param gene_symbol_of_interest
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_tissues_for_gene <- function(gene_symbol_of_interest) {
 
 gene_gencodeId_of_interest <- gtexr::get_genes(gene_symbol_of_interest) |>
