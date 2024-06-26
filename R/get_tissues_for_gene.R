7206cc03cae3d8dd3a713fc8ff0f101b5621066e
rmgpanw <rmgpanw@ucl.ac.uk>
2024-06-26 13:43:52 +0100

    address R CMD check

--- R/get_tissues_for_gene.R
+++ R/get_tissues_for_gene.R
@@ -2,19 +2,20 @@
 #'
 #' Get tissues where a gene has eQTLs.
 #'
-#' @param gene_symbol_of_interest
+#' @param gene_symbol_of_interest Character vector of gene symbols.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 get_tissues_for_gene <- function(gene_symbol_of_interest) {
+  gene_gencodeId_of_interest <- gtexr::get_genes(gene_symbol_of_interest) |>
+    dplyr::pull(dplyr::all_of("gencodeId")) |>
+    suppressMessages()
 
-gene_gencodeId_of_interest <- gtexr::get_genes(gene_symbol_of_interest) |>
-  dplyr::pull(gencodeId) |>
-  suppressMessages()
-
-gene_gencodeId_of_interest |>
-  gtexr::get_significant_single_tissue_eqtls() |>
-  dplyr::distinct(geneSymbol, gencodeId, tissueSiteDetailId)
+  gene_gencodeId_of_interest |>
+    gtexr::get_significant_single_tissue_eqtls() |>
+    dplyr::distinct(dplyr::across(dplyr::all_of(
+      c("geneSymbol", "gencodeId", "tissueSiteDetailId")
+    )))
 }
